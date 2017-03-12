7af68567011ac585d7ee2f3a96e90ca371cac329
jota_sk <jota_sk@hotmail.com>
2017-03-12 13:11:42 +0000

    refactoring and updated

--- core/src/com/github/jotask/neat/engine/WorldListener.java
+++ core/src/com/github/jotask/neat/engine/WorldListener.java
@@ -4,10 +4,6 @@ import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
-import com.github.jotask.neat.engine.entity.Enemy;
-import com.github.jotask.neat.engine.entity.Food;
-import com.github.jotask.neat.engine.radar.Radar;
-import com.github.jotask.neat.engine.radar.RadarEnemy;
 
 /**
  * WorldController
@@ -20,88 +16,11 @@ public class WorldListener implements ContactListener {
     @Override
     public void beginContact(Contact contact) {
 
-        Object a = contact.getFixtureA().getUserData();
-        Object b = contact.getFixtureB().getUserData();
-
-        if(a == null || b == null)
-            return;
-
-        if(a instanceof Enemy && b instanceof Enemy)
-            return;
-
-        if((a instanceof RadarEnemy && b instanceof Enemy) || (b instanceof RadarEnemy && a instanceof Enemy))
-            return;
-
-        if(a instanceof Food && b instanceof Food)
-            return;
-
-        if(a instanceof Radar && b instanceof Radar)
-            return;
-
-        Enemy enemy = null;
-        Food food = null;
-        RadarEnemy radar = null;
-
-        if(a instanceof Enemy){
-            enemy = (Enemy) a;
-        }else if(b instanceof Enemy){
-            enemy = (Enemy) b;
-        }
-        if(a instanceof Food){
-            food = (Food) a;
-        }else if(b instanceof Food){
-            food = (Food) b;
-        }
-        if(a instanceof RadarEnemy){
-            radar = (RadarEnemy) a;
-        }else if(b instanceof RadarEnemy){
-            radar = (RadarEnemy) b;
-        }
-
-        if(radar != null && food != null){
-            radar.inRange(food);
-        }else if(enemy != null && food != null){
-            enemy.eat(food);
-        }
-
     }
 
     @Override
     public void endContact(Contact contact) {
 
-        Object a = contact.getFixtureA().getUserData();
-        Object b = contact.getFixtureB().getUserData();
-
-        if(a == null || b == null)
-            return;
-
-
-        if(a instanceof Enemy || b instanceof Enemy)
-            return;
-
-        if(a instanceof Food && b instanceof Food)
-            return;
-
-        if(a instanceof Radar && b instanceof Radar)
-            return;
-
-        Food food = null;
-        RadarEnemy radar = null;
-
-        if(a instanceof Food){
-            food = (Food) a;
-        }else if(b instanceof Food){
-            food = (Food) b;
-        }
-        if(a instanceof RadarEnemy){
-            radar = (RadarEnemy) a;
-        }else if(b instanceof RadarEnemy){
-            radar = (RadarEnemy) b;
-        }
-
-        if(radar != null && food != null){
-            radar.outRange(food);
-        }
     }
 
     @Override
--- core/src/com/github/jotask/neat/engine/entity/Enemy.java
+++ core/src/com/github/jotask/neat/engine/entity/Enemy.java
@@ -4,9 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.github.jotask.neat.engine.Timer;
 import com.github.jotask.neat.engine.controller.EnemyController;
-import com.github.jotask.neat.jneat.Constants;
 
 /**
  * Enemy
@@ -22,13 +20,10 @@ public class Enemy extends Entity{
 
     public final float SPEED = 10;
 
-    private final Timer timer;
-
     public Enemy(final Body body) {
         super(body);
         this.controller = new EnemyController(this);
         this.velocity = new Vector2();
-        this.timer = new Timer(Constants.INIT_TIME);
     }
 
     @Override
@@ -40,16 +35,7 @@ public class Enemy extends Entity{
         this.getBody().applyForceToCenter(this.velocity.scl(SPEED), true);
 
         velocity.setZero();
-//        if(timer.isPassed()){
-//            this.die = true;
-//        }
 
-    }
-
-    public void eat(final Food food){
-        float getValue = food.getValue();
-        timer.add(getValue);
-        food.eaten();
     }
 
     @Override
