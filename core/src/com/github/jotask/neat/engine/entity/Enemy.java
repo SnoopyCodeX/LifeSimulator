54e77cd2f445eab4e826042f7de068a43e79b0b5
jota_sk <jota_sk@hotmail.com>
2017-03-20 17:42:51 +0000

    enemy holds weapons and weapons controllers

--- core/src/com/github/jotask/neat/engine/entity/Enemy.java
+++ core/src/com/github/jotask/neat/engine/entity/Enemy.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.github.jotask.neat.engine.controller.EnemyController;
+import com.github.jotask.neat.engine.controller.WeaponController;
+import com.github.jotask.neat.engine.weapon.Weapon;
 
 /**
  * Enemy
@@ -15,15 +17,20 @@ import com.github.jotask.neat.engine.controller.EnemyController;
 public class Enemy extends Entity{
 
     private final EnemyController controller;
+    private final WeaponController weaponController;
 
     public final Vector2 velocity;
 
     public final float SPEED = 10;
 
-    public Enemy(final Body body) {
+    private final Weapon weapon;
+
+    public Enemy(final Body body, final Weapon weapon) {
         super(body);
+        this.weapon = weapon;
         this.controller = new EnemyController(this);
         this.velocity = new Vector2();
+        this.weaponController = new WeaponController(weapon);
     }
 
     @Override
@@ -33,15 +40,24 @@ public class Enemy extends Entity{
     public void update() {
         this.getBody().applyForceToCenter(this.velocity.scl(SPEED), true);
         this.velocity.setZero();
+
+        this.weapon.update(this.getBody());
+
     }
 
     @Override
     public void debug(final ShapeRenderer sr){
+
+        if(this.weapon != null){
+            this.weapon.render(sr);
+        }
+
         sr.set(ShapeRenderer.ShapeType.Filled);
         sr.setColor(Color.RED);
         sr.circle(getBody().getPosition().x, getBody().getPosition().y, .5f, 20);
     }
 
-    public EnemyController getController() { return controller; }
+    protected EnemyController getController() { return this.controller; }
+    protected WeaponController getWeaponController(){ return this.weaponController; }
 
 }
